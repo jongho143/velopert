b8fde73781390bac8b08e86d1c80274670fb019d
leexiudev <leexiu.dev@gamil.com>
2020-04-09 14:48:36 +0900

    배열 데이터 렌더링 및 관리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import Counter from './Counter';
+import PhoneForm from './Component/PhoneForm';
+import PhoneInfoList from './Component/PhoneInfoList';
 
 
 
 class App extends Component {
+
+  id = 0;
+
+  state = {
+    information: [],
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({
+        ...data,
+        id: this.id++
+      })
+    })
+  }
+
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    });
+  }
   render() {
     return (
       <div>
-        <Counter />
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList
+          data={this.state.information}
+          onRemove={this.handleRemove}
+        />
       </div>
     );
   }
